6ec74504f675054b7bece19a749445f4fda4dcc6
dongho9 <donghobridge@gmail.com>
2025-07-21 14:23:49 +0900

    전체적인 수정

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const cursorPointed = document.querySelector(".pointed");
+
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+window.addEventListener("mousemove", moveCursor);
+
+// const links = document.querySelectorAll("a");
+
+// links.forEach((link) => {
+//   link.addEventListener("mouseenter", () => {
+//     cursorPointed.classList.add("active");
+//   });
+//   link.addEventListener("mouseleave", () => {
+//     cursorPointed.classList.remove("active");
+//   });
+// });
+
 // GSAP
 gsap.registerPlugin(ScrollTrigger);
 
@@ -47,9 +69,11 @@ const tl = gsap.timeline();
 const split = new SplitText(".headLine01", { type: "chars" });
 const split02 = new SplitText(".headLine02", { type: "chars" });
 const split03 = new SplitText(".con02 h3", { type: "chars" });
+const split04 = new SplitText("footer p", { type: "chars" });
 const mySplitText = split.chars; // 문자 배열
 const mySplitText02 = split02.chars; // 문자 배열
 const mySplitText03 = split03.chars; // 문자 배열
+const mySplitText04 = split04.chars; // 문자 배열
 // tl.to(con01, {
 //   height: "600px",
 //   duration: 1,
@@ -92,18 +116,6 @@ gsap.to(".con01", {
   },
 });
 
-// gsap.to("main", {
-//   background: "#111",
-//   color: "#fff",
-//   ease: "none", // scrub 시 꼭 필요
-//   scrollTrigger: {
-//     trigger: ".con02",
-//     start: "-300px center",
-//     end: "bottom center",
-//     scrub: true,
-//   },
-// });
-
 gsap.to("main", {
   width: "100%",
   ease: "none", // scrub 시 꼭 필요
@@ -122,7 +134,40 @@ gsap.to(".pinSection", {
     trigger: ".pinSection",
     pin: true,
     start: "top top",
-    end: "+=3000",
+    end: "+=2000",
+    scrub: true,
+  },
+});
+
+gsap.to(".con03", {
+  width: "100%",
+  top: "0",
+  ease: "none", // scrub 시 꼭 필요
+  scrollTrigger: {
+    trigger: ".sectionTitle",
+    start: "top top",
+    scrub: true,
+  },
+});
+
+gsap.to(".con04", {
+  width: "100%",
+  top: "0",
+  ease: "none", // scrub 시 꼭 필요
+  scrollTrigger: {
+    trigger: ".con03",
+    start: "top top",
+    scrub: true,
+  },
+});
+
+gsap.to(".con05", {
+  width: "100%",
+  top: "0",
+  ease: "none", // scrub 시 꼭 필요
+  scrollTrigger: {
+    trigger: ".con04",
+    start: "top top",
     scrub: true,
   },
 });
@@ -161,66 +206,8 @@ gsap.from(".con02 h3", {
   },
 });
 
-// gsap.to(".con02 > div", {
-//   duration: 0.6,
-//   width: "100%",
-//   ease: "power4.out",
-//   scrollTrigger: {
-//     trigger: ".con02",
-//     start: "-5% top", // .con02가 뷰포트 80% 지점에 닿을 때 실행
-//     end: "-5% top", // .con02가 뷰포트 80% 지점에 닿을 때 실행
-//     markers: true, // 디버깅용, 완료 후 제거 가능
-//   },
-// });
-
-// gsap.to(".con02 div p", {
-//   duration: 0.8,
-//   opacity: 1,
-//   delay: 0.3,
-//   scrollTrigger: {
-//     trigger: ".con02",
-//     start: "-5% top", // .con02가 뷰포트 80% 지점에 닿을 때 실행
-//     end: "-5% top", // .con02가 뷰포트 80% 지점에 닿을 때 실행
-//     markers: true, // 디버깅용, 완료 후 제거 가능
-//   },
-// });
-
-gsap.to(".con03", {
-  width: "100%",
-  top: "0",
-  ease: "none", // scrub 시 꼭 필요
-  scrollTrigger: {
-    trigger: ".sectionTitle",
-    start: "top top",
-    scrub: true,
-  },
-});
-
-gsap.to(".con04", {
-  width: "100%",
-  top: "0",
-  ease: "none", // scrub 시 꼭 필요
-  scrollTrigger: {
-    trigger: ".con03",
-    start: "top top",
-    scrub: true,
-  },
-});
-
-gsap.to(".con05", {
-  width: "100%",
-  top: "0",
-  ease: "none", // scrub 시 꼭 필요
-  scrollTrigger: {
-    trigger: ".con04",
-    start: "top top",
-    scrub: true,
-  },
-});
-
 const nav = document.querySelectorAll("header nav ul li a");
 const home = document.querySelector("header nav ul li:first-child");
-console.log(home);
 nav.forEach((item) => {
   item.addEventListener("click", (e) => {
     link = item.getAttribute("href");
@@ -232,16 +219,67 @@ home.addEventListener("click", () => {
   lenis.scrollTo(0, 0);
 });
 
-ScrollTrigger.create({
-  trigger: "#con02",
-  start: "top top",
-  end: "bottom top",
-  toggleClass: {
-    targets: ".header nav ul li:nth-child(2)",
-    className: "active",
+gsap.to("main", {
+  background: "#111",
+  color: "#fff",
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#con04",
+    end: "top top",
+    scrub: true,
   },
-  // markers: true, // 디버깅용
 });
+
+gsap.to("#con04 .titleText p:first-child", {
+  left: "0",
+  ease: "none", // scrub 시 꼭 필요
+  scrollTrigger: {
+    trigger: ".gridTitle",
+    end: "top top",
+    scrub: true,
+  },
+});
+
+gsap.to("#con04 .titleText p:last-child", {
+  right: "0",
+  ease: "none", // scrub 시 꼭 필요
+  scrollTrigger: {
+    trigger: ".gridTitle",
+    end: "top top",
+    scrub: true,
+  },
+});
+// gsap.to("main", {
+//   background: "#fff",
+//   color: "#111",
+//   ease: "none", // scrub 시 꼭 필요
+//   scrollTrigger: {
+//     trigger: "footer",
+//     end: "top top",
+//     scrub: true,
+//   },
+// });
+gsap.from(mySplitText04, {
+  y: 200,
+  skewY: 10,
+  duration: 0.6,
+  stagger: 0.1,
+  ease: "power4.out",
+  scrollTrigger: {
+    trigger: "footer",
+    end: "top top",
+    markers: true,
+  },
+});
+
+let windowWidth = window.innerWidth;
+
+window.addEventListener("resize", () => {
+  location.reload(); // 화면 너비 임계점 넘을 때만 새로고침
+  document.body.style.overflow = "auto";
+  window.scrollTo(0, 0);
+});
+
 const image01 = document.getElementsByClassName("thumbnail");
 new simpleParallax(image01, {
   scale: 1.2,
@@ -262,16 +300,42 @@ new simpleParallax(image03, {
 });
 
 const parallax = document.querySelectorAll(".simpleParallax");
-parallax[1].innerHTML = `
-  <img
-    class="thumbnail"
-    src="https://v6.usestate.org/images/home/select-image01.webp"
-    alt="clone01"
-  />
-  <div class="cloneText">
-    <p>HLL 중앙</p>
-  </div>
+
+parallax[1].innerHTML = `<img class="thumbnail"
+src="https://v6.usestate.org/images/home/select-image01.webp" alt=""/>
+<div class="cloneText"> 
+  <p>HLL</p>
+</div>
 `;
+
+parallax[2].innerHTML = `<img class="thumbnail"
+src="https://v6.usestate.org/images/home/select-image05.webp" alt=""/>
+<div class="cloneText"> 
+  <p>Republic Records</p>
+</div>
+`;
+
+parallax[3].innerHTML = `<img class="thumbnail"
+src="https://v6.usestate.org/images/home/select-image06.webp" alt=""/>
+<div class="cloneText"> 
+  <p>Musign</p>
+</div>
+`;
+
+parallax[4].innerHTML = `<img class="thumbnail"
+src="https://v6.usestate.org/images/home/select-image01.webp" alt=""/>
+<div class="cloneText"> 
+  <p>Spotify</p>
+</div>
+`;
+
+parallax[5].innerHTML = `<img class="thumbnail"
+src="https://v6.usestate.org/images/home/select-image04.webp" alt=""/>
+<div class="cloneText"> 
+  <p></p>
+</div>
+`;
+
 parallax[1].addEventListener("click", () => {
   window.open("https://dongho9.github.io/HLL/", "_blank");
 });
